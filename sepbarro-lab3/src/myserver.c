05959ab29aedec69d923ce447f8a1f28aefb8b55
Serjo Barron <sepbarro@ucsc.edu>
2024-02-17 00:31:13 -0800

    maked formatted

--- sepbarro-lab3/src/myserver.c
+++ sepbarro-lab3/src/myserver.c
@@ -9,15 +9,15 @@
 #define BUFFER_SIZE 1024
 
 char *get_timestamp() {
-    time_t rawtime;
-    struct tm *timeinfo;
-    static char timestamp[30];
+  time_t rawtime;
+  struct tm *timeinfo;
+  static char timestamp[30];
 
-    time(&rawtime);
-    timeinfo = localtime(&rawtime);
-    strftime(timestamp, sizeof(timestamp), "%Y-%m-%dT%H:%M:%S", timeinfo);
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+  strftime(timestamp, sizeof(timestamp), "%Y-%m-%dT%H:%M:%S", timeinfo);
 
-    return timestamp;
+  return timestamp;
 }
 
 // Function to process received packets
@@ -25,7 +25,7 @@ void process_packet(int sockfd, struct sockaddr_in *client_addr,
                     socklen_t addr_len, int droppc, char *outfile_path,
                     int *pktsn) {
   char buffer[BUFFER_SIZE];
-  //char log_message[100];
+  // char log_message[100];
 
   // Simulate packet drop based on droppc
   int should_drop = (rand() % 100) < droppc;
@@ -90,61 +90,66 @@ void process_packet(int sockfd, struct sockaddr_in *client_addr,
 }
 
 void start_server(int port, int droppc) {
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd == -1) {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
-    }
+  int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd == -1) {
+    perror("Socket creation failed");
+    exit(EXIT_FAILURE);
+  }
 
-    struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(port);
+  struct sockaddr_in server_addr;
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(port);
 
-    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-        perror("Socket bind failed");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Server listening on port %d\n", port);
-
-    struct sockaddr_in client_addr;
-    socklen_t addr_len = sizeof(client_addr);
-
-    char outfile_path[256] = "";
-    int pktsn = 0;
-
-    while (1) {
-        process_packet(sockfd, &client_addr, addr_len, droppc, outfile_path, &pktsn);
-        pktsn++;
-    }
-
+  if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) ==
+      -1) {
+    perror("Socket bind failed");
     close(sockfd);
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Server listening on port %d\n", port);
+
+  struct sockaddr_in client_addr;
+  socklen_t addr_len = sizeof(client_addr);
+
+  char outfile_path[256] = "";
+  int pktsn = 0;
+
+  while (1) {
+    process_packet(sockfd, &client_addr, addr_len, droppc, outfile_path,
+                   &pktsn);
+    pktsn++;
+  }
+
+  close(sockfd);
 }
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <Port Number> <Drop Percentage>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s <Port Number> <Drop Percentage>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
 
-    int port = atoi(argv[1]);
-    int droppc = atoi(argv[2]);
+  int port = atoi(argv[1]);
+  int droppc = atoi(argv[2]);
 
-    if (port <= 1023 || port > 65535) {
-        fprintf(stderr, "Invalid port number. Port must be in the range 1024-65535.\n");
-        exit(EXIT_FAILURE);
-    }
+  if (port <= 1023 || port > 65535) {
+    fprintf(stderr,
+            "Invalid port number. Port must be in the range 1024-65535.\n");
+    exit(EXIT_FAILURE);
+  }
 
-    if (droppc < 0 || droppc > 100) {
-        fprintf(stderr, "Invalid drop percentage. Percentage must be in the range 0-100.\n");
-        exit(EXIT_FAILURE);
-    }
+  if (droppc < 0 || droppc > 100) {
+    fprintf(
+        stderr,
+        "Invalid drop percentage. Percentage must be in the range 0-100.\n");
+    exit(EXIT_FAILURE);
+  }
 
-    srand(time(NULL));
-    start_server(port, droppc);
+  srand(time(NULL));
+  start_server(port, droppc);
 
-    return 0;
+  return 0;
 }
