e39ad147fe167faa4f42dd76c30df3ab9631b905
Serjo Barron <sepbarro@ucsc.edu>
2024-03-14 20:37:11 -0700

    updating code

--- sepbarro/src/myproxy.c
+++ sepbarro/src/myproxy.c
@@ -11,130 +11,131 @@
 #include <time.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 8192
+#define BUFFER_SIZE 4096 // KiB
 
-// Mutex for protecting access to forbidden sites list
-pthread_mutex_t mutex_forbidden_sites = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_forbidden_sites =
+    PTHREAD_MUTEX_INITIALIZER; // mutex for forbidden sites array
 
-// Global variables for forbidden sites and access log file paths
-char *forbidden_sites_file;
-char *access_log_file;
-char **forbidden_sites; // Array to store forbidden sites
+char *forbidden_sites_file; // global forbidden site file
+char *access_log_file;      // global access log file
+char **forbidden_sites;     // forbidden sites array
 int num_forbidden_sites = 0;
+
+void validport(int port) {
+  if (0 <= port && port <= 1023) {
+    fprintf(stderr, "Port number cannot be well-known: 0-1023\n");
+    exit(1);
+  } else if (port < 0) {
+    fprintf(stderr, "Port number cannot be negative\n");
+    exit(1);
+  } else if (port > 65535) {
+    fprintf(stderr, "Port number out of range, must be within: 1024-65535\n");
+    exit(1);
+  }
+  return;
+}
 
 void load_forbidden_sites() {
   FILE *file = fopen(forbidden_sites_file, "r");
   if (file == NULL) {
-    perror("Error opening forbidden sites file");
+    fprintf(stderr, "Error opening forbidden sites file\n");
     return;
   }
 
-  printf("Loading forbidden sites from file: %s\n", forbidden_sites_file);
+  //printf("Loading forbidden sites from file: %s\n", forbidden_sites_file);
 
-  // Allocate memory for a temporary array to store new forbidden sites
   char **new_forbidden_sites = NULL;
   int new_num_forbidden_sites = 0;
 
-  // Count the number of lines in the file
+  // count number of lines (to allocate memory for)
   int num_lines = 0;
   char buffer[2048];
   while (fgets(buffer, sizeof(buffer), file) != NULL) {
     num_lines++;
   }
 
-  printf("Number of lines in the file: %d\n", num_lines);
+  // printf("Number of lines in the file: %d\n", num_lines);
 
-  // Reset file pointer to beginning
-  fseek(file, 0, SEEK_SET);
+  fseek(file, 0, SEEK_SET); // reset pointer to start of file
 
-  // Allocate memory for the new forbidden_sites array
+  // allocate memory for new forbidden sites array
   new_forbidden_sites = (char **)malloc(num_lines * sizeof(char *));
   if (new_forbidden_sites == NULL) {
-    perror("Error allocating memory for new forbidden sites array");
+    fprintf(stderr, "Error allocating memory for new forbidden sites array\n");
     fclose(file);
     return;
   }
 
-  printf("Memory allocated for new forbidden_sites array\n");
+  //printf("Memory allocated for new forbidden_sites array\n");
 
-  // Read lines from the file and store them in the new_forbidden_sites array
   int i = 0;
   while (fgets(buffer, sizeof(buffer), file) != NULL) {
-    // Remove newline character from the end
     size_t len = strlen(buffer);
-    if (len > 0 && buffer[len - 1] == '\n') {
+    if (len > 0 &&
+        buffer[len - 1] == '\n') { // remove newline character (at end)
       buffer[len - 1] = '\0';
     }
     new_forbidden_sites[i] = strdup(buffer);
-    printf("Loaded forbidden site: %s\n", new_forbidden_sites[i]);
+    //printf("Loaded forbidden site: %s\n", new_forbidden_sites[i]);
     i++;
   }
 
   new_num_forbidden_sites = num_lines;
   fclose(file);
-  printf("New forbidden sites loaded successfully\n");
+  
+  //printf("New forbidden sites loaded successfully\n");
 
-  // Free memory allocated by the previous forbidden_sites array
+  // free memory of old forbidden sites array
   for (i = 0; i < num_forbidden_sites; i++) {
     free(forbidden_sites[i]);
   }
   free(forbidden_sites);
 
-  // Update the global forbidden_sites array and num_forbidden_sites
   pthread_mutex_lock(&mutex_forbidden_sites);
-  forbidden_sites = new_forbidden_sites;
-  num_forbidden_sites = new_num_forbidden_sites;
+  forbidden_sites = new_forbidden_sites; // update sites
+  num_forbidden_sites =
+      new_num_forbidden_sites; // update number of forbidden sites
   pthread_mutex_unlock(&mutex_forbidden_sites);
 
-  printf("Forbidden sites updated successfully\n");
+  return;
 }
 
-// Function to handle SIGINT signal (Control-C)
 void handle_sigint(int sig) {
-  (void)sig; // Suppress unused parameter warning
-  // Reload the forbidden sites file here
+  (void)sig;
   load_forbidden_sites();
   printf("Forbidden sites file reloaded.\n");
-
-  // exit(1); // remove later
+  return;
 }
 
-// Function to check if a given hostname or IP address is forbidden
 int is_forbidden(const char *hostname_or_ip) {
-  // pthread_mutex_lock(&mutex_forbidden_sites);
-  for (int i = 0; i < num_forbidden_sites; i++) {
+  for (int i = 0; i < num_forbidden_sites; i++) { // check if forbidden
     if (strstr(hostname_or_ip, forbidden_sites[i]) != NULL) {
-      // pthread_mutex_unlock(&mutex_forbidden_sites);
-      return 1; // Forbidden
+      exit(1);
     }
   }
-  // pthread_mutex_unlock(&mutex_forbidden_sites);
-  return 0; // Not forbidden
+  return 0; // host/ip not forbidden
 }
 
-// Function to parse HTTP request and extract hostname, path, and port
 int parse_http_request(const char *request, char *hostname, char *path,
                        int *port) {
   char method[10], url[2048], version[10];
 
-  // Parse request line to extract method, URI, and HTTP version
-  if (sscanf(request, "%s %s %s", method, url, version) != 3) {
-    // Invalid request format
+  if (sscanf(request, "%s %s %s", method, url, version) !=
+      3) { // if invalid format (missing info)
     return -1;
   }
 
-  // Check if the method is PUT
-  if (strcmp(method, "PUT") == 0) {
-    // Method not implemented (501)
-    return -1; // Stop processing the request
+  if (strcmp(method, "GET") != 0 &&
+      strcmp(method, "HEAD") != 0) { // method not implemented (501)
+    return -1;
   }
 
-  // Parse the URL to extract hostname, path, and port
+  // parse URL to get hostname, path, and port
   char *host_start = strstr(url, "//");
   if (host_start == NULL) {
     return -1;
   }
-  host_start += 2; // Move past "//"
+  host_start += 2; // move pointer past '//'
 
   char *host_end = strchr(host_start, '/');
   if (host_end == NULL) {
@@ -144,28 +145,27 @@ int parse_http_request(const char *request, char *hostname, char *path,
   strncpy(hostname, host_start, len);
   hostname[len] = '\0';
 
-  // Parse port if specified in the URL
+  // retrieve port (if part of URL)
   char *port_start = strchr(hostname, ':');
   if (port_start != NULL) {
     *port = atoi(port_start + 1);
     *port_start = '\0';
   } else {
-    *port = 80; // Default port
+    *port = 80; // default port
   }
 
-  // Copy the path
-  strcpy(path, host_end);
+  strcpy(path, host_end); // copy host path
 
-  return 0; // Successful parsing
+  return 0;
 }
 
-// Function to send HTTP response
 void send_response(int client_sock, const char *status, const char *headers,
                    const char *body) {
   char response[BUFFER_SIZE];
   snprintf(response, sizeof(response), "%s\r\n%s\r\n%s\r\n\r\n", status,
            headers, body);
   send(client_sock, response, strlen(response), 0);
+  return;
 }
 
 void log_request(const struct sockaddr_in *dest_addr, const char *method,
@@ -176,39 +176,38 @@ void log_request(const struct sockaddr_in *dest_addr, const char *method,
   char time_str[64];
   strftime(time_str, sizeof(time_str), "%Y-%m-%dT%H:%M:%S", tm_info);
 
-  char ip_str[INET_ADDRSTRLEN]; // Buffer to hold IP address string
+  char ip_str[INET_ADDRSTRLEN]; // IP address
 
-  // If dest_addr is not NULL, get the IP address string
-  if (dest_addr != NULL) {
+  if (dest_addr != NULL) { // get IP address string
     inet_ntop(AF_INET, &(dest_addr->sin_addr), ip_str, INET_ADDRSTRLEN);
-  } else {
-    // If dest_addr is NULL, set IP address string to "Unknown"
+  } else { // dest_addr is NULL, IP is unknown
     strcpy(ip_str, "Unknown");
   }
 
-  // Create log entry
+  // format log entry
   char log_entry[BUFFER_SIZE];
   snprintf(log_entry, sizeof(log_entry), "%s %s \"%s %s %s\" %d %zd\n",
            time_str, ip_str, method, uri, version, status_code, bytes_received);
 
-  // Write log entry to file
+  // write log entry to access log file
   FILE *access_log = fopen(access_log_file, "a");
   if (access_log != NULL) {
     fprintf(access_log, "%s", log_entry);
     fclose(access_log);
   }
+
+  return;
 }
 
-// Function to handle client request
 void *handle_client(void *arg) {
   int client_sock = *((int *)arg);
   free(arg);
 
-  // Handle client request
+  // receive client request
   char request_buffer[BUFFER_SIZE];
   recv(client_sock, request_buffer, sizeof(request_buffer), 0);
 
-  // Parse HTTP request
+  // parse incoming HTTP request
   char hostname[2048], uri[2048];
   int port;
   if (parse_http_request(request_buffer, hostname, uri, &port) != 0) {
@@ -218,7 +217,7 @@ void *handle_client(void *arg) {
     return NULL;
   }
 
-  // Check if hostname is forbidden
+  // check if hostname or IP is in forbidden array
   pthread_mutex_lock(&mutex_forbidden_sites);
   if (is_forbidden(hostname)) {
     pthread_mutex_unlock(&mutex_forbidden_sites);
@@ -229,20 +228,20 @@ void *handle_client(void *arg) {
   }
   pthread_mutex_unlock(&mutex_forbidden_sites);
 
-  // Forward request to destination server
+  // forward request to destination server
   int dest_sock = socket(AF_INET, SOCK_STREAM, 0);
   if (dest_sock < 0) {
-    perror("Socket creation failed");
+    fprintf(stderr, "Socket creation failed\n");
     send_response(client_sock, "HTTP/1.1 502 Bad Gateway", "", "");
     log_request(NULL, "GET", uri, "HTTP/1.1", 502, -1);
     close(client_sock);
     return NULL;
   }
 
-  // Resolve hostname
+  // domain name resolution
   struct hostent *dest_host = gethostbyname(hostname);
   if (dest_host == NULL) {
-    perror("Error resolving hostname");
+    fprintf(stderr, "Error resolving hostname\n");
     send_response(client_sock, "HTTP/1.1 502 Bad Gateway", "", "");
     log_request(NULL, "GET", uri, "HTTP/1.1", 502, -1);
     close(client_sock);
@@ -256,10 +255,9 @@ void *handle_client(void *arg) {
   dest_addr.sin_addr = *((struct in_addr *)dest_host->h_addr);
   dest_addr.sin_port = htons(port);
 
-  // Connect to destination server
   if (connect(dest_sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr)) <
       0) {
-    perror("Connection to destination server failed");
+    fprintf(stderr, "Connection to destination server failed\n");
     send_response(client_sock, "HTTP/1.1 504 Gateway Timeout", "", "");
     log_request(NULL, "GET", uri, "HTTP/1.1", 504, -1);
     close(client_sock);
@@ -267,15 +265,15 @@ void *handle_client(void *arg) {
     return NULL;
   }
 
-  // Send request to destination server
+  // send request to destination server
   send(dest_sock, request_buffer, strlen(request_buffer), 0);
 
-  // Receive response from destination server
+  // receive response from destination server
   char response_buffer[BUFFER_SIZE];
   ssize_t bytes_received =
       recv(dest_sock, response_buffer, sizeof(response_buffer), 0);
   if (bytes_received < 0) {
-    perror("Error receiving response from destination server");
+    fprintf(stderr, "Error receiving response from destination server\n");
     send_response(client_sock, "HTTP/1.1 502 Bad Gateway", "", "");
     log_request(NULL, "GET", uri, "HTTP/1.1", 502, -1);
     close(client_sock);
@@ -283,17 +281,16 @@ void *handle_client(void *arg) {
     return NULL;
   }
 
-  // Send response back to client
+  // send response to client
   send(client_sock, response_buffer, bytes_received, 0);
 
-  // Log the request
+  // log request
   char method[10], version[10];
   if (sscanf(request_buffer, "%s %s %s", method, uri, version) != 3) {
     strcpy(uri, "InvalidRequest");
   }
   log_request(&dest_addr, method, uri, "HTTP/1.1", 200, bytes_received);
 
-  // Close connections
   close(client_sock);
   close(dest_sock);
 
@@ -305,25 +302,23 @@ int main(int argc, char *argv[]) {
     fprintf(stderr,
             "Usage: %s listen_port forbidden_sites_file access_log_file\n",
             argv[0]);
-    return 1;
+    exit(1);
   }
 
-  // Set up SIGINT handler
-  signal(SIGINT, handle_sigint);
+  signal(SIGINT, handle_sigint); // SIGINT handler
 
-  // Parse command line arguments
   int listen_port = atoi(argv[1]);
   forbidden_sites_file = argv[2];
   access_log_file = argv[3];
 
-  // Load forbidden sites from file
-  load_forbidden_sites();
+  validport(listen_port); // check if port is within valid range
 
-  // Set up socket for listening
+  load_forbidden_sites(); // load forbidden sites (initial load)
+
   int server_sock = socket(AF_INET, SOCK_STREAM, 0);
   if (server_sock < 0) {
-    perror("Socket creation failed");
-    return 1;
+    fprintf(stderr, "Socket creation failed\n");
+    exit(1);
   }
 
   struct sockaddr_in server_addr;
@@ -333,16 +328,15 @@ int main(int argc, char *argv[]) {
 
   if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) <
       0) {
-    perror("Binding failed");
-    return 1;
+    fprintf(stderr, "Binding failed\n");
+    exit(1);
   }
-
   if (listen(server_sock, 10) < 0) {
-    perror("Listening failed");
-    return 1;
+    fprintf(stderr, "Listening failed\n");
+    exit(1);
   }
 
-  printf("Proxy server listening on port %d...\n", listen_port);
+  printf("Proxy server listening on port: %d\n", listen_port);
 
   while (1) {
     struct sockaddr_in client_addr;
@@ -351,21 +345,20 @@ int main(int argc, char *argv[]) {
     *client_sock =
         accept(server_sock, (struct sockaddr *)&client_addr, &client_len);
     if (*client_sock < 0) {
-      perror("Accepting connection failed");
+      fprintf(stderr, "Accepting connection failed\n");
       continue;
     }
 
-    // Create a new thread to handle the client request
+    // thread for handling client requests
     pthread_t tid;
     if (pthread_create(&tid, NULL, handle_client, (void *)client_sock) != 0) {
-      perror("Failed to create thread");
+      fprintf(stderr, "Failed to create thread\n");
       close(*client_sock);
       free(client_sock);
       continue;
     }
 
-    // Detach the thread to allow it to run independently
-    pthread_detach(tid);
+    pthread_detach(tid); // detach thread
   }
 
   // all status codes supported: (these should also be logfileable)
@@ -380,10 +373,6 @@ int main(int argc, char *argv[]) {
   // 3/14/2024
   // todays tasks:
   // make document
-  // make sure all error cases
-  // rename vars
-  // rename errors
-  // add more error checking (e.g. port ranges)
   close(server_sock);
   return 0;
 }
